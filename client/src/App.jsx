652a081188f7851d69a7842a5c47f9510eb98df3
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-10-11 15:50:55 +0200

    Routes successfully working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,31 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import SignIn from './pages/signIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import About from './pages/About';
 
 function App() {
  
 
   return (
-    <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+   
+      <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      <Route path='/about' element={<About/>}/>
+      </Routes>
 
 
-    </>
+    </BrowserRouter>
+   
   )
 }
 
