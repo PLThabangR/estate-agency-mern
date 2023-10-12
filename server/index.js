d7e463ef2565b65ee5ecccc76be943009e169b3a
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-10-12 10:59:54 +0200

    connect to database

--- server/index.js
+++ server/index.js
@@ -1,8 +1,24 @@
 import express from "express";
+import connectDB from "./src/mongoDB/connect.js";
+import dotenv from "dotenv";
 
 const app = express();
+//initialize dotenv
+dotenv.config();
 
-app.listen(3000, ()=>{
-    console.log("Server is listening on 3000");
+const MONGO_URL="mongodb+srv://thabang:thabang@mern-agency.tgen2fp.mongodb.net/?retryWrites=true&w=majority"
+const startServer = ()=>{
+    try {
+   // connectDB(MONGO_URL);
+    app.listen(3000, ()=>{
+        console.log("Server is listening on port https://localhost:3000");
     
-});
+    });
+    }catch(error){
+        console.log(error);
+
+    }
+
+}
+
+startServer();
