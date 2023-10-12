dd1cc05fee0a9cb61b60eb1e6cf4acfdf8b7c383
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-10-12 14:17:13 +0200

    complete sign-up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ const Header = () => {
     <Link to='/about'>
     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
     </Link>
-    <Link to='/profile'>
+    <Link to='/sign-up'>
     <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
     </Link>
     </ul>
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const SignIn = () => {
   return(
-    <div>S</div>
+    <div>Sign in</div>
   )
 }
 
