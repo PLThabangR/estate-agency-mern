7c164f611fcfdc3de70123e3b398ec130a29635b
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-10-12 10:02:33 +0200

    Link router added to header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,11 +5,12 @@ const Header = () => {
   return (
     <div className='bg-slate-200 shadow-md' >
     <div className='flex justify-between items-center max-w-6xl mx-auto p-4'>
+    <Link to="/">
     <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
     <span className='text-slate-500'> TR</span>
     <span className='text-slate-700'> Housing</span>
     </h1>
-
+    </Link>
 
     
     <form className='bg-slate-100 p-2 rounded-lg flex items-center' >
@@ -20,9 +21,15 @@ const Header = () => {
     </form>
 
     <ul className='flex gap-3'>
+    <Link to='/'>
     <li className='hidden sm:inline text-slate-700 hover:underline' >Home</li>
+    </Link>
+    <Link to='/about'>
     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+    </Link>
+    <Link to='/profile'>
     <li className='hidden sm:inline text-slate-700 hover:underline'>Sign in</li>
+    </Link>
     </ul>
 
     </div>
