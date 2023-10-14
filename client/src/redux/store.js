fb9341a4ba1af1989deea88bbc6660a5767ae3be
Thabang Rakgoropo <rakgoropothabang@gmail.com>
2023-10-14 13:05:01 +0200

    Added user to the local storage using redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,23 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {configureStore,combineReducers} from '@reduxjs/toolkit';
 import userReducer from './userSlice';
+//import redux-persist to add to local storage
+import {persistReducer, persistStore} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const rootReducer= combineReducers({
+    user:userReducer
+});
+const persistConfig ={
+    key:'root',
+    storage,
+    version:1
+}
+
+const persistedReducer = persistReducer(persistConfig,rootReducer);
 
 export const store = configureStore({
-    reducer:{user: userReducer},
+    //reducer:{user: userReducer},
+    reducer:persistedReducer,
     //Add the middleware to prevent browser errors
     middleware: (getDefaultMiddleware)=>
         getDefaultMiddleware({
@@ -12,3 +26,6 @@ export const store = configureStore({
     
 
 })
+
+//Export the store persistor this gonna make the store to persist
+export const persistor = persistStore(store)
